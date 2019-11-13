0f27ca5431cfffc92a473505500ffd3b53e1f9bb
soft91 <soft4750@gmail.com>
2019-11-13 18:03:09 +0900

    Component Styling Ex

---
+++ react-example/component-styling-ex/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Letter from './Letter';
+
+ReactDOM.render(
+    <div>
+        <Letter bgcolor = "#58B3FF">A</Letter>
+        <Letter bgcolor = "#FF605F">E</Letter>
+        <Letter bgcolor = "#FFD52E">I</Letter>
+        <Letter bgcolor = "#49DD8E">O</Letter>
+        <Letter bgcolor = "#AE99FF">U</Letter>
+    </div>,
+    document.getElementById('root')
+);
