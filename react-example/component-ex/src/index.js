34cf475c2fad457799398360254c66dfca908f63
soft91 <soft4750@gmail.com>
2019-11-14 16:54:23 +0900

    Add Card.js (속성 전달 연습)

--- react-example/component-ex/src/index.js
+++ react-example/component-ex/src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Buttonify from './Buttonify';
-import * as serviceWorker from './serviceWorker';
+import Card from './Card';
 
 ReactDOM.render(
     <div>
-        <Buttonify behavior = "submit">Send Data</Buttonify>
+        <Card color = "#FF6663"/>
     </div>,
     document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
