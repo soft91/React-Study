50c8e5b68b437e9355d3a3c4d8b113b5e101b67e
soft91 <soft4750@gmail.com>
2019-11-15 23:13:54 +0900

    Component-LifeCycle-Ex

---
+++ react-example/component-lifecycle-ex/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CounterParent from './Counter';
+
+
+ReactDOM.render(
+    <CounterParent />,
+    document.getElementById('root')
+);
