c28cea08c0312347a5de180325dc7fe5e763c9e6
soft91 <soft4750@gmail.com>
2019-11-17 16:55:04 +0900

    React-Router-Ex

---
+++ react-example/react-router-ex/src/Contact.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Contact extends Component {
+    render(){
+        return(
+            <div>
+                <h2>GOT QUESTIONS?</h2>
+                <p>Contact Page</p>
+            </div>
+        )
+    }
+}
+
+export default Contact;
