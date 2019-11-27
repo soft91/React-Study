7d005c9f8cea0e5c1d43451235f406d273c5e638
soft91 <soft4750@gmail.com>
2019-11-27 17:17:00 +0900

    Redux-React 추가

---
+++ react-api-ex/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import createBrowserHistory from 'history/createBrowserHistory';
+import App from './App';
+import createStore from './createStore';
+
+const history = createBrowserHistory();
+const store = createStore(history);
+
+ReactDOM.render(
+    <Provider store = {store}>
+        <ConnectedRouter>
+            <App />
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root')
+);
