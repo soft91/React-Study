b5046c9cc245bf61a32e856285fa7d8bce8e2519
soft91 <soft4750@gmail.com>
2020-08-23 15:46:36 +0900

    react redux ex

---
+++ react-example/react-redux-ex2/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import ReduxApp from './components/ReduxApp01';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ReduxApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
