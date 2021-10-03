69eb499034b408c03879ee9bdff59d048b18201a
hsyoon <soft4750@gmail.com>
2021-10-03 18:07:49 +0900

    Styled Component 추가

--- react-todo/src/App.tsx
+++ react-todo/src/App.tsx
@@ -1,63 +1,45 @@
 import React from "react";
-import logo from './logo.svg';
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
+import { Button, Input } from "Components";
 
 const Container = styled.div`
-  text-align: center;
-`;
-
-const Header = styled.header`
-  background-color: #282c34 !important;
   min-height: 100vh;
   display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
+  flex-direction: column;
 `;
 
-const spin = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`
-
-const AppLogo = styled.img`
-  height: 40vmin;
-  pointer-events: none;
-
-  @media (prefers-reduced-motion: no-preference) {
-    animation: ${spin} infinite 20s linear;
-  }
+const Contents = styled.div`
+  display: flex;
+  background-color: #FFFFFF;
+  flex-direction: column;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
 `;
 
-const AppLink = styled.a`
-  color: #61dafb;
+const InputContainer = styled.div`
+  display: flex;
 `;
 
 function App() {
   return (
-    <Container className="App">
-      <Header className="App-header">
-        <AppLogo src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <AppLink
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </AppLink>
-      </Header>
+    <Container>
+      <Contents>
+        <InputContainer>
+          <Input 
+            placeholder="할 일을 입력해 주세요"
+            onChange={(text) => console.log(text)}
+          />
+          <Button 
+            label="테스트"
+            onClick={() => alert('test')}
+          />
+        </InputContainer>
+      </Contents>
     </Container>
-  );
+  )
 }
 
 export default App;
