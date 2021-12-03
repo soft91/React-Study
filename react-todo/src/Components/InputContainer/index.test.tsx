30b14a7bbbc1223700770958610150161e6ffb9c
hsyoon <soft4750@gmail.com>
2021-12-03 21:13:30 +0900

    InputConatiner, PageHeader test

--- react-todo/src/Components/InputContainer/index.test.tsx
+++ react-todo/src/Components/InputContainer/index.test.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import 'jest-styled-components';
 
 import { InputContainer } from './index';
 
 describe('<InputContinaer />', () => {
-  it('renders component correctly', () => {
-    const { container } = render(<InputContainer />);
+  it('calls the onAdd function when the user clicks Add button', () => {
+    const handleClick = jest.fn();
+    render(<InputContainer onAdd={handleClick} />);
 
     const input = screen.getByPlaceholderText('할 일을 입력해 주세요.');
-    expect(input).toBeInTheDocument();
     const button = screen.getByText('추가');
-    expect(button).toBeInTheDocument();
+    expect(handleClick).toHaveBeenCalledTimes(0);
 
-    expect(container).toMatchSnapshot();
+    fireEvent.click(button);
+    expect(handleClick).toHaveBeenCalledTimes(0);
+
+    fireEvent.change(input, { target: { value: 'study react 1' } });
+    fireEvent.click(button);
+    expect(handleClick).toHaveBeenCalledTimes(1);
   })
 })
