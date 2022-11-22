9fb55c0d5f593d9eec1bb1c33756dbb6f722aa06
Ethan <hsyoon@smallticket.com>
2022-11-22 15:50:15 +0900

    Recoil Test

---
+++ recoil-test/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { RecoilRoot } from "recoil";
+import CharacterCounter from "./components/CharacterCounter";
+import "./App.css";
+
+function App() {
+  return (
+    <RecoilRoot>
+      <CharacterCounter />
+    </RecoilRoot>
+  );
+}
+
+export default App;
