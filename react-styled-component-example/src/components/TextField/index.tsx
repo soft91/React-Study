aedbb0bc2a97f993b4ebecef347acb4abdc023a7
soft4750 <soft4750@gmail.com>
2022-12-17 23:11:24 +0900

    Fix: TextField Component 수정

--- react-styled-component-example/src/components/TextField/index.tsx
+++ react-styled-component-example/src/components/TextField/index.tsx
@@ -1,86 +1,79 @@
-import React, { Dispatch, SetStateAction, useCallback } from "react";
+import React, { ChangeEvent } from "react";
 import styled from "styled-components";
 
 interface IProps {
-  id?: string;
-  name?: string;
-  value: string;
-  setValue: Dispatch<SetStateAction<string>>;
-  placeholder?: string;
-  disabled?: boolean;
-  size?: string;
+	id?: string;
+	name?: string;
+	value: string;
+	setValue: (e: ChangeEvent<HTMLInputElement>) => void;
+	placeholder?: string;
+	disabled?: boolean;
+	size?: string;
 }
 
 const Container = styled.div<{ size?: string }>`
-  display: flex;
-  align-items: center;
-  width: ${(props) => selectSize(props.size).width};
-  height: ${(props) => selectSize(props.size).height};
-  margin: 5px;
-  border: 2px solid black;
-  border-radius: 5px;
+	display: flex;
+	align-items: center;
+	width: ${(props) => selectSize(props.size).width};
+	height: ${(props) => selectSize(props.size).height};
+	margin: 5px;
+	border: 2px solid black;
+	border-radius: 5px;
 `;
 
 const Input = styled.input<{ size?: string }>`
-  padding-left: 5px;
-  outline: none;
-  border: 0px;
-  width: 100%;
-  border-radius: 5px;
-  font-size: ${(props) => selectSize(props.size).fontSize};
+	padding-left: 5px;
+	outline: none;
+	border: 0px;
+	width: 100%;
+	border-radius: 5px;
+	font-size: ${(props) => selectSize(props.size).fontSize};
 `;
 
 const selectSize = (size?: string) => {
-  switch (size) {
-    case "large":
-      return {
-        height: "50px",
-        width: "200px",
-        fontSize: "20px",
-      };
-    case "small":
-      return {
-        height: "30px",
-        width: "100px",
-        fontSize: "15px",
-      };
-    default:
-      return {
-        height: "40px",
-        width: "150px",
-      };
-  }
+	switch (size) {
+		case "large":
+			return {
+				height: "50px",
+				width: "200px",
+				fontSize: "20px",
+			};
+		case "small":
+			return {
+				height: "30px",
+				width: "100px",
+				fontSize: "15px",
+			};
+		default:
+			return {
+				height: "40px",
+				width: "150px",
+			};
+	}
 };
 
 const TextField = ({
-  id,
-  name,
-  value,
-  setValue,
-  placeholder,
-  size,
-  disabled = false,
+	id,
+	name,
+	value,
+	setValue,
+	placeholder,
+	size,
+	disabled = false,
 }: IProps) => {
-  const onChangehandler = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      setValue(e.target.value);
-    },
-    [setValue]
-  );
-
-  return (
-    <Container size={size}>
-      <Input
-        type="text"
-        id={id}
-        name={name}
-        value={value}
-        placeholder={placeholder}
-        disabled={disabled}
-        onChange={(e) => onChangehandler(e)}
-      />
-    </Container>
-  );
+	return (
+		<Container size={size}>
+			<Input
+				type="text"
+				id={id}
+				name={name}
+				value={value}
+				placeholder={placeholder}
+				disabled={disabled}
+				onChange={setValue}
+			/>
+		</Container>
+	);
 };
 
 export default TextField;
