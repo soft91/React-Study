e4723fac428d15637ff8f6f4e7394ffe72cb7dc5
soft4750 <soft4750@gmail.com>
2023-01-03 22:23:57 +0900

    feat: 버튼 컴포넌트

--- react-styled-component-example/.storybook/preview.js
+++ react-styled-component-example/.storybook/preview.js
@@ -1,9 +1,20 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+
+export const decorators = [
+	(Story) => (
+		<ThemeProvider theme="default">
+			<Story />
+		</ThemeProvider>
+	),
+];
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+	actions: { argTypesRegex: "^on[A-Z].*" },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+};
