792fe46df357b518a19cdf01f4dd1eae90e73568
soft4750 <soft4750@gmail.com>
2023-01-05 21:23:11 +0900

    feat: commit

--- react-styled-component-example/src/components/button/Button.stories.tsx
+++ react-styled-component-example/src/components/button/Button.stories.tsx
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
 import React from "react";
 import Button from "./index";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import { IProps } from "./types";
-
-export default {
-	title: "Button",
-	component: Button,
-} as ComponentMeta<typeof Button>;
-
-//👇 We create a “template” of how args map to rendering
-=======
-import { ComponentMeta, ComponentStory } from "@storybook/react";
-import Button from "./index";
 import { IProps } from "./types";
 
 export default {
@@ -20,22 +8,15 @@ export default {
 	title: "Example/Button",
 } as ComponentMeta<typeof Button>;
 
->>>>>>> 9583e20ce185da5df500ae9367a6bc0deefdba5a
 const Template: ComponentStory<typeof Button> = (args: IProps) => (
 	<Button {...args} />
 );
-
-<<<<<<< HEAD
-export const Primary = Template.bind({});
-Primary.args = {
-	children: "test",
-};
 
 export const Active = Template.bind({});
 Active.args = {
 	children: "test",
 	theme: "active",
-=======
+};
 export const Default = Template.bind({});
 Default.args = {
 	children: "",
@@ -53,5 +34,4 @@ Large.args = {
 export const Small = Template.bind({});
 Large.args = {
 	size: "small",
->>>>>>> 9583e20ce185da5df500ae9367a6bc0deefdba5a
 };
