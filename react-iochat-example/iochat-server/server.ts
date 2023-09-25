0c204c87686310db2e700296af530bce54166aa8
soft4750 <soft4750@gmail.com>
2023-09-25 22:54:15 +0900

    feat: gitignore 추가

---
+++ react-iochat-example/iochat-server/server.ts
@@ -0,0 +1,19 @@
+const { Server } = require("socket.io");
+
+const io = new Server("5000", {
+	cors: {
+		origin: "http://localhost:3000",
+	},
+});
+
+io.sockets.on("connection", (socket) => {
+	socket.on("message", (data) => {
+		io.sockets.emit("sMessage", data);
+	});
+	socket.on("login", (data) => {
+		io.sockets.emit("sLogin", data);
+	});
+	socket.on("disconnect", () => {
+		console.log("user disconnected");
+	});
+});
