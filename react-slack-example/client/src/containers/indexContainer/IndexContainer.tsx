8744de0d48723add3cc1558c2e426dd5fd2b306d
soft91 <soft4750@gmail.com>
2024-02-18 21:17:13 +0900

    feat: maincontainer 추가

---
+++ react-slack-example/client/src/containers/indexContainer/IndexContainer.tsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import { css } from "@emotion/react";
+import {
+	indexContainerCss,
+	loginWrapCss,
+	headerCss,
+	loginFormCss,
+	inputCss,
+	btnCss,
+} from "./indexContainer.style";
+import { socket, socketPrivate, socketGroup } from "../../socket";
+import { useNavigate } from "react-router-dom";
+import logo from "../../images/logo.png";
+
+const IndexContainer: React.FC = () => {
+	const [user, setUser] = useState<string>("");
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		socket.on("connect_error", (err) => {
+			if (err.message === "invalid userId") {
+				console.log("err");
+			}
+		});
+	}, []);
+
+	const onLoginHandler = (e: React.FormEvent) => {
+		e.preventDefault();
+		if (!user) return;
+		socket.auth = { userId: user };
+		socket.connect();
+		socketPrivate.auth = { userId: user };
+		socketPrivate.connect();
+		socketGroup.auth = { userId: user };
+		socketGroup.connect();
+		navigate("/main");
+	};
+
+	const onUserNameHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setUser(e.target.value);
+	};
+
+	return (
+		<div css={indexContainerCss}>
+			<div css={loginWrapCss}>
+				<h1 css={headerCss}>
+					<img src={logo} width="100px" height="auto" alt="logo" />
+				</h1>
+				<form css={loginFormCss} onSubmit={onLoginHandler}>
+					<input
+						css={inputCss}
+						type="text"
+						value={user}
+						placeholder="Enter your ID"
+						onChange={onUserNameHandler}
+					/>
+					<button type="submit" css={btnCss}>
+						Sign in
+					</button>
+				</form>
+			</div>
+		</div>
+	);
+};
+
+export default IndexContainer;
