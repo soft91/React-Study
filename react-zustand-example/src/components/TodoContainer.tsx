65a192f15b1583151984d33b4997959fd753385e
soft91 <soft4750@gmail.com>
2025-04-15 22:17:39 +0900

    feat: Todo LIst

--- react-zustand-example/src/components/TodoContainer.tsx
+++ react-zustand-example/src/components/TodoContainer.tsx
@@ -1,9 +1,11 @@
 import { TodoInput } from "./TodoInput";
+import { TodoList } from "./TodoLIst";
 
 export const TodoContainer = () => {
 	return (
 		<div className="flex flex-col items-center py-4 px-4">
 			<TodoInput />
+			<TodoList />
 		</div>
 	);
 };
--- react-zustand-example/src/components/TodoLIst.tsx
+++ react-zustand-example/src/components/TodoLIst.tsx
@@ -1,1 +1,47 @@
-export {};
+import { useTodoStore } from "../store/todoStore";
+import { useShallow } from "zustand/shallow";
+
+export const TodoList = () => {
+	const { todos, toggleTodo, removeTodo } = useTodoStore(
+		useShallow((state) => ({
+			todos: state.todos,
+			toggleTodo: state.toggleTodo,
+			removeTodo: state.removeTodo,
+		}))
+	);
+
+	return (
+		<div className="flex-1 justify-center items-center gap-2">
+			<ul className="space-y-2 mt-4">
+				{todos.map((todo) => (
+					<li
+						key={todo.id}
+						className="flex items-center justify-between bg-white p-4 rounded-xl shadow-sm hover:shadow-md transition"
+					>
+						<div className="flex items-center space-x-3 w-full">
+							<input
+								type="checkbox"
+								checked={todo.completed}
+								onChange={() => toggleTodo(todo.id)}
+								className="w-5 h-5 text-blue-500 rounded border-gray-300 focus:ring-blue-400"
+							/>
+							<span
+								className={`text-gray-800 ${
+									todo.completed ? "line-through text-gray-400" : ""
+								}`}
+							>
+								{todo.text}
+							</span>
+						</div>
+						<button
+							onClick={() => removeTodo(todo.id)}
+							className="text-red-500 hover:text-red-600 transition"
+						>
+							&times;
+						</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
