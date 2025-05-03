df47c7ea5459d46342e2adc74d519a40d1a30c00
soft91 <soft4750@gmail.com>
2025-05-03 21:32:02 +0900

    feat: 마무리

--- react-zustand-example/src/App.tsx
+++ react-zustand-example/src/App.tsx
@@ -2,9 +2,6 @@ import { TodoContainer } from "./components/TodoContainer";
 import { Toaster } from "@/components/ui/sonner";
 import { Alert } from "./components/ui/Alert";
 
-import "./styles/globals.css";
-import "./App.css";
-
 function App() {
 	return (
 		<div className="container mx-auto">
